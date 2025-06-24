de7e8495dbd326d28f82cc8748d9e9efbe4ff23d
YuliiaDubyniuk <yuliia.dubyniuk@gmail.com>
2025-06-24 08:13:04 -0400

    Added function calls

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def get_days_from_today(date: str) -> int:
     return days_difference
 
 
+get_days_from_today("2025-10-09")
+
+
 """Task two"""
 
 
@@ -47,6 +50,10 @@ def get_numbers_ticket(min_num: int, max_num: int, quantity: int) -> list:
         return sorted_numbers
 
 
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+
 """Task three"""
 
 
@@ -66,6 +73,22 @@ def normalize_phone(phone_number: str) -> str:
             return '+38' + cleaned_phone
         case _:
             print(f"Invalid phone number: {phone_number}.")
+
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
 
 
 """Task four"""
@@ -118,3 +141,13 @@ def get_birthday_greeting_date(birthday: date) -> date:
     elif weekday == 6:  # birthday is on Sunday
         birthday += timedelta(days=1)
     return birthday
+
+
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.08.27"},
+    {"name": "Oliver Brown", "birthday": "1993.06.29"}
+]
+
+upcoming_birthdays = get_upcoming_birthdays(users)
+print("Список привітань на цьому тижні:", upcoming_birthdays)
